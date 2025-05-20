8bbd136e7d163a03c5af98eca3411d8945521023
Nicolás T López <nicolas.torreslop@gmail.com>
2025-05-19 23:43:05 -0500

    feature(cocktailDetails): create the model to cocktailDetails

---
+++ src/app/domains/shared/models/cocktailDetails.model.ts
@@ -0,0 +1,11 @@
+export interface CocktailDetails {
+    idDrink: number;
+    strDrink: string;
+    strCategory: string;
+    strAlcoholic: string;
+    strGlass: string;
+    strInstructions: string;
+    strDrinkThumb: string;
+    strIngredient : string[];
+    strMeasure : string[];
+}
