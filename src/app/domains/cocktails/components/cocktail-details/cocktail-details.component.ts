9bac9beb3d4603029109849c3003c5ae838934c8
Nicolás T López <nicolas.torreslop@gmail.com>
2025-05-25 21:28:57 -0500

    feat(cocktailDetails): create cocktail detail view and modify the service to search details for cocktail

--- src/app/domains/cocktails/components/cocktail-details/cocktail-details.component.ts
+++ src/app/domains/cocktails/components/cocktail-details/cocktail-details.component.ts
@@ -1,11 +1,41 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, signal } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { CocktailDetails } from '../../../shared/models/cocktailDetails.model';
+import { CocktailService } from '../../../shared/services/cocktail.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cocktail-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cocktail-details.component.html',
   styleUrl: './cocktail-details.component.css'
 })
 export class CocktailDetailsComponent {
   @Input() idDrink: number | null = null;
+
+  cocktailDetails =  signal({} as CocktailDetails); 
+  private cocktailDetailSubscribe : Subscription | null = null;
+  private cocktailDetailService = inject(CocktailService);
+
+  ngOnInit(): void {
+
+    this.cocktailDetailSubscribe = this.cocktailDetailService.getCocktailById( this.idDrink )
+      .subscribe({
+        next: ( res : CocktailDetails )  => {
+          this.cocktailDetails.set(res);
+        },
+        error: ( err : any ) => {
+          console.error('Error fetching cocktail details:', err);
+          this.cocktailDetails.set({} as CocktailDetails);
+        }
+      });
+
+  }
+
+  ngOnDestroy(): void {
+    if( this.cocktailDetailSubscribe ){
+      this.cocktailDetailSubscribe.unsubscribe();
+    }
+  }
+    
 }
